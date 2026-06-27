fb727e643431dc1f78d0adb81a9a1a6a80913b90
Taashvi Aggarwal <taashviaggarwal06@gmail.com>
2026-06-27 11:49:07 +0530

    Fix Railway Port

--- app.py
+++ app.py
@@ -286,7 +286,10 @@ def download_pdf():
 # ===========================
 # Run App
 # ===========================
-
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
